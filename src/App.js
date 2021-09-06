ca93433ce4ea717dcbeeba8453fb0bd6a7289a34
farhandardiri <faran.f4124n@gmail.com>
2021-09-06 13:12:20 +0700

    Map JSON Review

--- src/App.js
+++ src/App.js
@@ -3,19 +3,58 @@ import React from 'react';
 import './App.css';
 
 function App() {
-
-      return ( 
+ return ( 
     <div className="Parentbox"> 
-    <FotoProduk/>
-    <ProdukInfo isDiscount="comming" category="lebaran" name="KOPI"/>
+        <FotoProduk/>
+        <ProdukInfo isDiscount="comming" category="lebaran" name="KOPI"/>
+        <ReviewItems/>
     </div>
     );
 }
 
+function ReviewItems(){
+    // mempersiapkan data dummy JSON
+    const users=[
+        {
+            "id":1,
+            "name":"Blink",
+            "photo":"img1.png",
+            "review":"Harganya murah tapi kualitas bukan kaleng-kaleng nih. keren."
+        },
+        {
+            "id":2,
+                "name":"Dhani",
+                "photo":"img2.png",
+                "review":"Harganya murah tapi kualitas bukan kaleng-kaleng nih. keren."
+        },
+        {
+            "id":3,
+                "name":"Isat",
+                "photo":"img3.png",
+                "review":"Harganya murah tapi kualitas bukan kaleng-kaleng nih. keren."
+        }
+    ]
+    const ListReview= users.map((itemReview) =>
+    <div className="Item">
+    <img src={itemReview.photo}/>
+    <div className="User">
+        <h3>{itemReview.name}</h3>
+        <p>{itemReview.review}</p>
+    </div>
+</div>
+  );
+  return(
+    <div className="Review-box">
+            <h2>Reviews</h2>
+           {ListReview}
+        </div>
+      )
+}
+
 function FotoProduk(){
-    return(
+ return(
     <div className="Foto">
-        <img src="Dick and Carey.jpg"/>
+        <img src="Biji Kopi.png"/>
     </div>
     )
 }
