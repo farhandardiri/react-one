38c721ce740e2543ad15821ce82e229ef08cd3b9
farhandardiri <faran.f4124n@gmail.com>
2021-09-06 13:21:22 +0700

    Split & import Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import ReviewItems from './Review'
 
 function App() {
  return ( 
@@ -10,45 +11,6 @@ function App() {
         <ReviewItems/>
     </div>
     );
-}
-
-function ReviewItems(){
-    // mempersiapkan data dummy JSON
-    const users=[
-        {
-            "id":1,
-            "name":"Blink",
-            "photo":"img1.png",
-            "review":"Harganya murah tapi kualitas bukan kaleng-kaleng nih. keren."
-        },
-        {
-            "id":2,
-                "name":"Dhani",
-                "photo":"img2.png",
-                "review":"Harganya murah tapi kualitas bukan kaleng-kaleng nih. keren."
-        },
-        {
-            "id":3,
-                "name":"Isat",
-                "photo":"img3.png",
-                "review":"Harganya murah tapi kualitas bukan kaleng-kaleng nih. keren."
-        }
-    ]
-    const ListReview= users.map((itemReview) =>
-    <div className="Item">
-    <img src={itemReview.photo}/>
-    <div className="User">
-        <h3>{itemReview.name}</h3>
-        <p>{itemReview.review}</p>
-    </div>
-</div>
-  );
-  return(
-    <div className="Review-box">
-            <h2>Reviews</h2>
-           {ListReview}
-        </div>
-      )
 }
 
 function FotoProduk(){
---
+++ src/Review.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+
+
+function Review(){
+    // mempersiapkan data dummy JSON
+    const users=[
+        {
+            "id":1,
+            "name":"Blink",
+            "photo":"img1.png",
+            "review":"Harganya murah tapi kualitas bukan kaleng-kaleng nih. keren."
+        },
+        {
+            "id":2,
+                "name":"Dhani",
+                "photo":"img2.png",
+                "review":"Harganya murah tapi kualitas bukan kaleng-kaleng nih. keren."
+        },
+        {
+            "id":3,
+                "name":"Isat",
+                "photo":"img3.png",
+                "review":"Harganya murah tapi kualitas bukan kaleng-kaleng nih. keren."
+        }
+    ]
+    const ListReview= users.map((itemReview) =>
+    <div className="Item">
+    <img src={itemReview.photo}/>
+    <div className="User">
+        <h3>{itemReview.name}</h3>
+        <p>{itemReview.review}</p>
+    </div>
+</div>
+  );
+  return(
+    <div className="Review-box">
+            <h2>Reviews</h2>
+           {ListReview}
+        </div>
+      )
+}
+
+export default Review;
